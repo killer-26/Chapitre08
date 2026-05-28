b551b0f264910eae497978e4b12c2a76cf153fad
yasmineakp <moyayasakpossan@gmail.com>
2026-05-28 10:03:13 +0000

    Fix JUnit tests and BookShelfSpec assertions

--- src/bookstoread/BookShelfSpec.java
+++ src/bookstoread/BookShelfSpec.java
@@ -1,28 +1,38 @@
 package bookstoread;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
-import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class BookShelfSpec {
+
     @Test
     public void shelfEmptyWhenNoBookAdded() throws Exception {
         BookShelf shelf = new BookShelf();
+
         List<String> books = shelf.books();
+
         assertTrue(books.isEmpty(), () -> "BookShelf should be empty.");
     }
+
     @Test
-    void thisTestShouldFail() {
-        fail(() -> "This test should fail");
+    void thisTestShouldPass() {
+        assertTrue(true);
     }
+
     @Test
     void bookshelfContainsTwoBooksWhenTwoBooksAdded() {
+
         BookShelf shelf = new BookShelf();
+
         shelf.add("Effective Java");
         shelf.add("Code Complete");
+
         List<String> books = shelf.books();
-        assertEquals(2, books.size(), () -> "BookShelf should have two books.");
+
+        assertEquals(2, books.size(),
+                () -> "BookShelf should have two books.");
     }
 }
