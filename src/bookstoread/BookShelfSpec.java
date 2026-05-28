028f88ba3fccb927eda6891e01db5911acd15aa7
yasmineakp <moyayasakpossan@gmail.com>
2026-05-28 09:54:37 +0000

    init commit

--- src/bookstoread/BookShelfSpec.java
+++ src/bookstoread/BookShelfSpec.java
@@ -13,4 +13,16 @@ public class BookShelfSpec {
         List<String> books = shelf.books();
         assertTrue(books.isEmpty(), () -> "BookShelf should be empty.");
     }
+    @Test
+    void thisTestShouldFail() {
+        fail(() -> "This test should fail");
+    }
+    @Test
+    void bookshelfContainsTwoBooksWhenTwoBooksAdded() {
+        BookShelf shelf = new BookShelf();
+        shelf.add("Effective Java");
+        shelf.add("Code Complete");
+        List<String> books = shelf.books();
+        assertEquals(2, books.size(), () -> "BookShelf should have two books.");
+    }
 }
